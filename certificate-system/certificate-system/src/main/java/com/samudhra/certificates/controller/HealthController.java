d0a64560ba70c80fc9120a9eb826d0c3e6b68fd0
prathap0983 <prathapranger@gmail.com>
2026-07-22 21:56:06 +0530

    Set Hikari timeout to allow startup without active DB connection

---
+++ certificate-system/certificate-system/src/main/java/com/samudhra/certificates/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.samudhra.certificates.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "App is running";
+    }
+}
